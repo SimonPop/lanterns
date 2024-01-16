c90f70b550746d2b1ffb9b55e3434f4be23f0ba2
Simon Popelier <simon.popelier@gmail.com>
2024-01-16 23:26:43 +0100

    Update documentation

--- js/nucleotide_attention.js
+++ js/nucleotide_attention.js
@@ -1,7 +1,7 @@
  // set the dimensions and margins of the graph
  var margin = {top: 0, right: 30, bottom: 50, left: 60},
  width = 650 - margin.left - margin.right,
- height = 400 - margin.top - margin.bottom;
+ height = 200 - margin.top - margin.bottom;
  
  // append the svg object to the body of the page
  var svg = d3.select("#my_dataviz")
